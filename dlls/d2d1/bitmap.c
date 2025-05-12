e89cb5206c21b289d10a04c1ab21ed10d98bc3a3
Makarenko Oleg <oleg@makarenk.ooo>
2025-05-13 02:11:57 +0300

    Fix DrawBitmap

--- dlls/d2d1/bitmap.c
+++ dlls/d2d1/bitmap.c
@@ -854,7 +854,7 @@ void d2d_bitmap_pop_layer(struct d2d_bitmap *bitmap, struct d2d_device_context *
     };
 
     ID2D1DeviceContext_DrawBitmap(context_iface, entry->target, &rect,
-            entry->params.opacity, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL);
+            entry->params.opacity, D2D1_BITMAP_INTERPOLATION_MODE_LINEAR, NULL, NULL);
 
     ID2D1Bitmap1_Release(entry->target);
     ID2D1Layer_Release(entry->layer);
