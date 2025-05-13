b4dca60a47a641ebad2e5f1c56cea117635dcf11
Makarenko Oleg <oleg@makarenk.ooo>
2025-05-13 20:50:43 +0300

    Layers stub

--- dlls/d2d1/bitmap.c
+++ dlls/d2d1/bitmap.c
@@ -777,6 +777,9 @@ struct d2d_bitmap *unsafe_impl_from_ID2D1Bitmap(ID2D1Bitmap *iface)
 void d2d_bitmap_push_layer(struct d2d_bitmap *bitmap, struct d2d_device_context *context,
         const D2D1_LAYER_PARAMETERS1 *params, ID2D1Layer *layer)
 {
+    WARN("Push layer stub\n");
+    return;
+
     struct d2d_layer_entry entry;
     ID2D1DeviceContext *context_iface = (ID2D1DeviceContext *)&context->ID2D1DeviceContext6_iface;
     ID2D1Bitmap1 *bitmap_current = (ID2D1Bitmap1 *)&bitmap->ID2D1Bitmap1_iface;
@@ -838,6 +841,9 @@ void d2d_bitmap_push_layer(struct d2d_bitmap *bitmap, struct d2d_device_context 
 
 void d2d_bitmap_pop_layer(struct d2d_bitmap *bitmap, struct d2d_device_context *context)
 {
+    WARN("Pop layer stub\n");
+    return;
+
     ID2D1DeviceContext *context_iface = (ID2D1DeviceContext *)&context->ID2D1DeviceContext6_iface;
     ID2D1Bitmap1 *bitmap_current = (ID2D1Bitmap1 *)&bitmap->ID2D1Bitmap1_iface;
 
