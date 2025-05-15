b9f4fa4800e8d93d9820be8ae416648b2bf28af8
Makarenko Oleg <oleg@makarenk.ooo>
2025-05-15 11:33:05 +0300

    Set constant TextScaleFactor as 1

--- dlls/windows.ui/uisettings.c
+++ dlls/windows.ui/uisettings.c
@@ -217,6 +217,7 @@ DEFINE_IINSPECTABLE( uisettings2, IUISettings2, struct uisettings, IUISettings_i
 static HRESULT WINAPI uisettings2_get_TextScaleFactor( IUISettings2 *iface, DOUBLE *value )
 {
     FIXME( "iface %p, value %p stub!\n", iface, value );
+    if (value) *value = 1;
     return E_NOTIMPL;
 }
 
