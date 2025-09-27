c42b07e49e67e3b69cde7552ec1399d3393638d3
Oleg Makarenko <oleg@makarenk.ooo>
2025-09-27 21:09:29 +0300

    d2d1: add Layers implementation

--- dlls/d2d1/d2d1_private.h
+++ dlls/d2d1/d2d1_private.h
@@ -62,6 +62,13 @@ struct d2d_settings
 };
 extern struct d2d_settings d2d_settings;
 
+struct d2d_layer_stack
+{
+    struct d2d_layer **stack; // stack of pointers to the d2d_layer
+    size_t size;
+    size_t count;
+};
+
 struct d2d_clip_stack
 {
     D2D1_RECT_F *stack;
@@ -218,7 +225,7 @@ struct d2d_device_context
 
     D2D1_RENDER_TARGET_PROPERTIES desc;
     D2D1_SIZE_U pixel_size;
-
+    struct d2d_layer_stack layer_stack;
 
     struct d2d_indexed_objects vertex_buffers;
 };
@@ -405,10 +412,20 @@ struct d2d_layer
     LONG refcount;
 
     ID2D1Factory *factory;
+
+    D2D1_LAYER_PARAMETERS1 params;
+    D2D1_SIZE_U pixel_size;
+    ID2D1Image *prev_target;
+    ID2D1Bitmap1 *offscreen_bitmap;
+
+    D2D1_MATRIX_3X2_F prev_transform;
+
     D2D1_SIZE_F size;
 };
 
 HRESULT d2d_layer_create(ID2D1Factory *factory, const D2D1_SIZE_F *size, struct d2d_layer **layer);
+struct d2d_layer *unsafe_impl_from_ID2D1Layer(ID2D1Layer *iface);
+
 
 struct d2d_mesh
 {
